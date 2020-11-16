3d4b2f8b03b66b196fa27b58df4301dd63a09720
Mihail Marian <mihailmarian12@gmail.com>
2020-11-16 13:59:23 +0200

    Added file-loader to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 module.exports = {
   entry: './src/app.js',
   output: {
-      path: __dirname + '/dist',
-      filename: 'build.js',
+    path: __dirname + '/dist',
+    filename: 'build.js',
   },
   module: {
-      loaders: [
+    loaders: [
+      {
+          test: /\.scss$/,
+          loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
           {
-              test: /\.scss$/,
-              loader: 'style-loader!css-loader!sass-loader'
-          },
-      ],
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ],
   },
   watch: true,
 };
